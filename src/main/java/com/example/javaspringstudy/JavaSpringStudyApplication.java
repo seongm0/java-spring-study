c13b48c9fe5bae47565cc54284476dd053207ddf
seongmo <eodah4115@gmail.com>
2023-03-22 17:37:16 +0900

    initial commit

---
+++ src/main/java/com/example/javaspringstudy/JavaSpringStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaspringstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaspringstudy/JavaSpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
